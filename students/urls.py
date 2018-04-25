f967d27dc321b1ae582330c0b7f26d1ec116f85a
butko <MaksimButko@nixsolutions.com>
2018-04-25 18:58:10 +0300

    Add create student functionality to frontend

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,6 @@ from students import views as students_views
 app_name = 'students'
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/', students_views.student_detail, name='detail'),
-    url(r'^$', students_views.list_view, name='list_view')
+    url(r'^add/$', students_views.student_add, name='student_add'),
+    url(r'^$', students_views.list_view, name='list_view'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseNotFound
+from django.contrib import messages
 from students.models import Student
+from students.forms import ModelStudentForm
 
 
 def list_view(request):
@@ -17,3 +19,18 @@ def student_detail(request, pk):
     except Student.DoesNotExist:
         return HttpResponseNotFound()
     return render(request, 'students/detail.html', {'student': student})
+
+
+def student_add(request):
+    if request.method == 'POST':
+        model_form = ModelStudentForm(request.POST)
+        if model_form.is_valid():
+            model_form.save()
+            messages.success(request, 'Студент добавлен!')
+
+            return redirect('/students/')
+        else:
+            messages.error(request, 'ну сука заполни же ты поля!')
+    else:
+        model_form = ModelStudentForm()
+    return render(request, 'students/add.html', {'model_form': model_form})
