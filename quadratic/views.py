991b2364405851ca7dc7d3cbc5df78d2ea4b3ff2
butko <MaksimButko@nixsolutions.com>
2018-04-25 13:37:31 +0300

    Контрольное задание 7.2

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,80 +1,95 @@
 from django.shortcuts import render
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class QuadraticForm(forms.Form):
 
-    a = forms.CharField(max_length=10)
-    b = forms.CharField(max_length=10)
-    c = forms.CharField(max_length=10)
+    a = forms.CharField(
+        label='коэффициент a:',
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex='^[-]?[0-9]*$',
+                message='коэффициент не целое число',
+                code='invalid_value'
+            ),
+            RegexValidator(
+                regex='^[^0]*$',
+                message='коэффициент при первом слагаемом уравнения не может быть равным нулю',
+                code='invalid_value'
+            ),
+        ]
+    )
+    b = forms.CharField(
+        label='коэффициент b:',
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex='^[-]?[0-9]*$',
+                message='коэффициент не целое число',
+                code='invalid_value'
+            )
+        ]
+    )
+    c = forms.CharField(
+        label='коэффициент c:',
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex='^[-]?[0-9]*$',
+                message='коэффициент не целое число',
+                code='invalid_value'
+            )
+        ]
+    )
 
 
 def quadratic_results(request):
-    args = collect_args(request)
-    context = validate(args)
     form = QuadraticForm()
-    context['form'] = form
-    if not context['error']:
-        context['descr'] = get_descriminant(context)
-    if not context['error']:
-        context['result_text'] = get_result_text(context)
+    discriminant = result_text = {}
+    if request.GET:
+        form = QuadraticForm(request.GET)
 
-    return render(request, "quadratic/results.html", context)
+        if (form.is_valid()):
+            arguments = get_form_arguments(form)
+            discriminant = get_discriminant(arguments)
+            result_text = get_result_text(discriminant, arguments)
 
-
-def collect_args(request):
-    args = dict()
-    args['a'] = (request.GET['a'])
-    args['b'] = (request.GET['b'])
-    args['c'] = (request.GET['c'])
-
-    return args
+    return render(request, "quadratic/results.html", {
+        'form': form,
+        'discriminant': discriminant,
+        'result_text': result_text
+    })
 
 
-def get_descriminant(arguments):
-    return arguments['b']['val'] ** 2 - 4 * arguments['a']['val'] * arguments['c']['val']
+def get_discriminant(arguments):
+    return int(arguments['b']) ** 2 - 4 * int(arguments['a']) * int(arguments['c'])
 
 
-def get_result_text(arguments):
-    if arguments['descr'] == 0:
-        x = calc_root(arguments)
+def get_form_arguments(form):
+    arguments = {}
+    for element in form.data:
+        arguments[element] = int(form.data[element])
+
+    return arguments
+
+
+def get_result_text(discriminant, arguments):
+    if discriminant == 0:
+        x = calc_root(discriminant, arguments)
         return 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = ' + str(x)
-    elif arguments['descr'] < 0:
+    elif discriminant < 0:
         return 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
     else:
-        x1 = calc_root(arguments)
-        x2 = calc_root(arguments, 2)
+        x1 = calc_root(discriminant, arguments)
+        x2 = calc_root(discriminant, arguments, 2)
         return 'Квадратное уравнение имеет два действительных корня: x1 = ' + str(x1) + ', x2 = ' + str(x2)
 
 
-def calc_root(arguments, order=1):
+def calc_root(discriminant, arguments, order=1):
     if order == 1:
-        x = (-arguments['b']['val'] + arguments['descr'] ** (1 / 2.0)) / 2 * arguments['a']['val']
+        x = (-arguments['b'] + discriminant ** (1 / 2.0)) / 2 * arguments['a']
     else:
-        x = (-arguments['b']['val'] - arguments['descr'] ** (1 / 2.0)) / 2 * arguments['a']['val']
+        x = (-arguments['b'] - discriminant ** (1 / 2.0)) / 2 * arguments['a']
     return x
-
-
-def validate(args):
-    error = False
-    resp = {}
-    for key in args:
-        err_message = ''
-        val = args[key]
-        if not val:
-            err_message = 'коэффициент не определен'
-            error = True
-        else:
-            try:
-                val = int(val)
-                if key == 'a' and val == 0:
-                    err_message = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-                    error = True
-            except ValueError:
-                err_message = 'коэффициент не целое число'
-                error = True
-
-        resp[key] = {'val': val, 'err_message': err_message}
-    resp['error'] = error
-
-    return resp
