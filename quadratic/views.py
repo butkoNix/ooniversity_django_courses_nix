288cf1fca6210dfacb2676c175014b36f43c26e3
butko <MaksimButko@nixsolutions.com>
2018-04-25 18:55:13 +0300

    Move quadratic form do direct file

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,48 +1,5 @@
 from django.shortcuts import render
-from django import forms
-from django.core.validators import RegexValidator
-
-
-class QuadraticForm(forms.Form):
-
-    a = forms.CharField(
-        label='коэффициент a:',
-        max_length=10,
-        validators=[
-            RegexValidator(
-                regex='^[-]?[0-9]*$',
-                message='коэффициент не целое число',
-                code='invalid_value'
-            ),
-            RegexValidator(
-                regex='^[^0]*$',
-                message='коэффициент при первом слагаемом уравнения не может быть равным нулю',
-                code='invalid_value'
-            ),
-        ]
-    )
-    b = forms.CharField(
-        label='коэффициент b:',
-        max_length=10,
-        validators=[
-            RegexValidator(
-                regex='^[-]?[0-9]*$',
-                message='коэффициент не целое число',
-                code='invalid_value'
-            )
-        ]
-    )
-    c = forms.CharField(
-        label='коэффициент c:',
-        max_length=10,
-        validators=[
-            RegexValidator(
-                regex='^[-]?[0-9]*$',
-                message='коэффициент не целое число',
-                code='invalid_value'
-            )
-        ]
-    )
+from quadratic.forms import QuadraticForm
 
 
 def quadratic_results(request):
